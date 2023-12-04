5e36308f7567e50649f8ae8bf5b3635105059087
madakelet <mileaddy@gmail.com>
2023-12-04 09:26:55 +0100

    day1 and day2 solutions added

---
+++ day2/Game.java
@@ -0,0 +1,94 @@
+package day2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Game {
+    private int gameNr;
+    private List<Round> rounds;
+    private final int REDCUBES = 12;
+    private final int GREENCUBES = 13;
+    private final int BLUECUBES = 14;
+
+    public Game(int gameNumber) {
+        this.gameNr = gameNumber;
+        this.rounds = new ArrayList<>();
+    }
+
+    public void addRound(int number, String color) {
+        rounds.add(new Round(number, color));
+    }
+
+    public void processInput(String input) {
+        String[] roundStrings = input.split(";");
+
+        for (String roundString : roundStrings) {
+            roundString = roundString.trim();
+            Matcher matcher = Pattern.compile("(\\d+)\\s+(\\w+)").matcher(roundString);
+
+            while (matcher.find()) {
+                int number = Integer.parseInt(matcher.group(1));
+                String color = matcher.group(2);
+                addRound(number, color);
+            }
+        }
+    }
+
+    public boolean isPossible() {
+        System.out.println("Game Number: " + gameNr);
+        boolean possible = true;
+        int i = 0;
+        while (possible && i < rounds.size()) {
+            Round round = rounds.get(i);
+            if (round.getColor().equals("red") && round.getNumber() > REDCUBES) {
+                System.out.println("Failed at " + round.getColor() + " : " + round.getNumber());
+                possible = false;
+            } else if (round.getColor().equals("blue") && round.getNumber() > BLUECUBES) {
+                System.out.println("Failed at " + round.getColor() + " : " + round.getNumber());
+                possible = false;
+            } else if (round.getColor().equals("green") && round.getNumber() > GREENCUBES) {
+                System.out.println("Failed at " + round.getColor() + " : " + round.getNumber());
+                possible = false;
+            }
+            i++;
+        }
+        return possible;
+    }
+
+    public int getPower() {
+        int greenMax = 0;
+        int redMax = 0;
+        int blueMax = 0;
+        for (int i = 0; i < rounds.size(); i++) {
+            Round round = rounds.get(i);
+            if (round.getColor().equals("red") && round.getNumber() > redMax) {
+                redMax = round.getNumber();
+            } else if (round.getColor().equals("blue") && round.getNumber() > blueMax) {
+                blueMax = round.getNumber();
+            } else if (round.getColor().equals("green") && round.getNumber() > greenMax) {
+                greenMax = round.getNumber();
+            }
+        }
+        return greenMax * blueMax * redMax;
+    }
+
+    private static class Round {
+        private int number;
+        private String color;
+
+        public Round(int number, String color) {
+            this.number = number;
+            this.color = color;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
+}
---
+++ day2/Main.java
@@ -0,0 +1,34 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("day2/input.txt"));
+            String line;
+            int sum = 0;
+            int powerSum = 0;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = Pattern.compile("Game (\\d+):").matcher(line);
+                int gameNumber = matcher.find() ? Integer.parseInt(matcher.group(1)) : -1;
+                Game game = new Game(gameNumber);
+                game.processInput(line);
+                if (game.isPossible()) {
+                    sum += gameNumber;
+                }
+                powerSum += game.getPower();   
+            }
+            reader.close();
+            System.out.println("Colors bigger sum: " + sum);
+            System.out.println("Colors power sum: " + powerSum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
