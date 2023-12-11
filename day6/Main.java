0be3815aaea59853e977ff67c3e35ebca9f97093
madakelet <mileaddy@gmail.com>
2023-12-11 13:54:52 +0100

    day6 part 2

--- day6/Main.java
+++ day6/Main.java
@@ -10,12 +10,21 @@ public class Main {
     public static int[] times;
     public static int[] distances;
     public static int possibleWinsMultiplied = 1;
+    public static long timesConcatenated;
+    public static long distancesConcatenated;
+    public static long sumOfConcatenated;
 
     public static void main(String[] args) {
         String filePath = "day6/input.txt";
         collectInputToArray(filePath);
         calculatePossibleWins();
         System.out.println("Possible wins multiplied = " + possibleWinsMultiplied);
+        timesConcatenated = concatNumbers(times);
+        distancesConcatenated = concatNumbers(distances);
+        System.out.println("Times concatenated = " + timesConcatenated);
+        System.out.println("Distances concatenated = " + distancesConcatenated);
+        calculatePartTwo();
+        System.out.println("Possible wins summed = " + sumOfConcatenated);
     }
 
     public static void collectInputToArray(String filePath) {
@@ -58,5 +67,21 @@ public class Main {
             }
             possibleWinsMultiplied *= numberOfWaysToWin;
         }
+    }
+
+    public static void calculatePartTwo() {
+        long inner = (timesConcatenated * timesConcatenated) - (4 * distancesConcatenated);
+        double rootOfInner = Math.sqrt(inner);
+        double bound = Math.abs((timesConcatenated * -1 + rootOfInner) / -2);
+        sumOfConcatenated = (long) (timesConcatenated - (2 * bound)) + 1;
+    }
+
+    public static long concatNumbers(int[] array) {
+        StringBuilder concatenated = new StringBuilder();
+        for (int number : array) {
+            concatenated.append(number);
+        }
+
+        return Long.parseLong(concatenated.toString());
     }
 }
