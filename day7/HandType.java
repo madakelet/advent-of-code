e4cdb74d7d407af6953edc9287b3119814f7b99e
madakelet <mileaddy@gmail.com>
2023-12-11 15:19:01 +0100

    solved day7 part 1

---
+++ day7/HandType.java
@@ -0,0 +1,11 @@
+package day7;
+
+public enum HandType {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIRS,
+    THREE_OF_A_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    FIVE_OF_A_KIND,
+}
---
+++ day7/Main.java
@@ -0,0 +1,41 @@
+package day7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static List<Hand> hands;
+
+    public static void main(String[] args) {
+        long sum = 0;
+        hands = new ArrayList<>();
+        createHansArrayFromInput("day7/input.txt");
+        Collections.sort(hands);
+        int ranking = 1;
+        for (Hand hand : hands) {
+            sum += hand.bid * ranking;
+            ranking++;
+        }
+        System.out.println("Sum of all bids: " + sum);
+    }
+
+    public static void createHansArrayFromInput(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(" ");
+                int bid = Integer.parseInt(values[1]);
+                String cards = values[0];
+                Hand hand = new Hand(cards, bid);
+                hands.add(hand);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
