5d714ccf170123313bff74e13af0d4ca0a0c8780
madakelet <mileaddy@gmail.com>
2023-12-14 22:18:44 +0100

    day5 task1

---
+++ day5/Range.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class Range {
+    long start;
+    long end;
+    long length;
+    long distance;
+
+    public Range(long destination, long start, long length) {
+        this.start = start;
+        this.length = length;
+        this.end = start + length - 1;
+        this.distance = destination - start;
+    }
+}
