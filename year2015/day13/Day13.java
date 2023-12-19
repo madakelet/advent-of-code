17756971c8e3448cb16a303ca5f7591e5b6775e0
madakelet <mileaddy@gmail.com>
2023-12-19 20:44:52 +0100

    day13

---
+++ year2015/day13/Day13.java
@@ -0,0 +1,88 @@
+package year2015.day13;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day13 {
+    static Map<String, Map<String, Integer>> happinessMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        String lines = utils.ReadFile.readFromFile("year2015/day13/input.txt");
+        processLines(lines);
+        List<String> people = new ArrayList<>(happinessMap.keySet());
+        List<List<String>> permutations = generatePermutations(people);
+        int maxHappiness = 0;   
+        for (List<String> permutation : permutations) {
+            int happiness = calculateHappiness(permutation);
+            if (happiness > maxHappiness) {
+                maxHappiness = happiness;
+            }
+        }
+        System.out.println(maxHappiness);
+    }
+
+    static void processLines(String lines) {
+        for (String line : lines.split("\n")) {
+            String[] parts = line.split(" ");
+            String from = parts[0];
+            String to = parts[10].substring(0, parts[10].length() - 1);
+            int happiness = Integer.parseInt(parts[3]);
+            if (parts[2].equals("lose")) {
+                happiness *= -1;
+            }
+            if (!happinessMap.containsKey(from)) {
+                happinessMap.put(from, new HashMap<>());
+            }
+            Map<String, Integer> fromMap = happinessMap.get(from);
+            fromMap.put(to, fromMap.getOrDefault(to, 0) + happiness);
+            if (!happinessMap.containsKey(to)) {
+                happinessMap.put(to, new HashMap<>());
+            }
+            Map<String, Integer> toMap = happinessMap.get(to);
+            toMap.put(from, toMap.getOrDefault(from, 0) + happiness);
+        }
+    }
+
+    static void printHappinessMap() {
+        for (String from : happinessMap.keySet()) {
+            for (String to : happinessMap.get(from).keySet()) {
+                System.out.println(from + " -> " + to + " = " + happinessMap.get(from).get(to));
+            }
+        }
+    }
+
+    static List<List<String>> generatePermutations(List<String> people) {
+        List<List<String>> result = new ArrayList<>();
+        permute(people.size() - 1, people, result);
+        return result;
+    }
+
+    static void permute(int n, List<String> people, List<List<String>> result) {
+        if (n == 0) {
+            result.add(new ArrayList<>(people));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            Collections.swap(people, i, n);
+            permute(n - 1, people, result);
+            Collections.swap(people, i, n);
+        }
+    }
+
+    static int calculateHappiness(List<String> people) {
+        int happiness = 0;
+        int i = 0;
+        while(i < people.size()) {
+            String from = people.get(i);
+            String to = people.get((i + 1) % people.size());
+            happiness += happinessMap.get(from).get(to);
+            happiness += happinessMap.get(to).get(from);
+            i++;
+        }
+        return happiness / 2;
+    }
+}
