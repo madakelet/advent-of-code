6104b1078cb1bf4f2e02b8594a25108a9c99a88e
madakelet <mileaddy@gmail.com>
2024-01-08 21:39:27 +0100

    17 day1 part2

--- src/year2017/day1/Day1.java
+++ src/year2017/day1/Day1.java
@@ -4,11 +4,15 @@ import utils.ReadFile;
 
 public class Day1 {
 
-    static int countNumbers(String input) {
+    static int countNumbers(String input, boolean partOne) {
         int sum = 0;
         for (int i = 0; i < input.length(); i++) {
             char current = input.charAt(i);
-            char next = input.charAt((i + 1) % input.length());
+            char next;
+            if (partOne)
+                next = input.charAt((i + 1) % input.length());
+            else
+                next = input.charAt((i + (input.length() / 2)) % input.length());
             if (current == next) {
                 sum += Character.getNumericValue(current);
             }
@@ -19,6 +23,7 @@ public class Day1 {
     public static void main(String[] args) {
         String input = ReadFile.readFromFile("src/year2017/day1/input.txt");
         input = input.trim();
-        System.out.println("Part 1: " + countNumbers(input));
+        System.out.println("Part 1: " + countNumbers(input, true));
+        System.out.println("Part 2: " + countNumbers(input, false));
     }
 }
--- tests/year2017/day1/Day1Test.java
+++ tests/year2017/day1/Day1Test.java
@@ -6,21 +6,43 @@ import org.junit.Test;
 
 public class Day1Test {
     @Test
-    public void testCountNumbers() {
+    public void testCountNumbersPartOne() {
         String input = "1122";
         int expected = 3;
-        assertEquals(expected, year2017.day1.Day1.countNumbers(input));
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, true));
 
         input = "1111";
         expected = 4;
-        assertEquals(expected, year2017.day1.Day1.countNumbers(input));
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, true));
 
         input = "1234";
         expected = 0;
-        assertEquals(expected, year2017.day1.Day1.countNumbers(input));
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, true));
 
         input = "91212129";
         expected = 9;
-        assertEquals(expected, year2017.day1.Day1.countNumbers(input));
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, true));
+    }
+    @Test
+    public void testCountNumbersPartTwo() {
+        String input = "1212";
+        int expected = 6;
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, false));
+
+        input = "1221";
+        expected = 0;
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, false));
+
+        input = "123425";
+        expected = 4;
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, false));
+
+        input = "123123";
+        expected = 12;
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, false));
+
+        input = "12131415";
+        expected = 4;
+        assertEquals(expected, year2017.day1.Day1.countNumbers(input, false));
     }
 }
